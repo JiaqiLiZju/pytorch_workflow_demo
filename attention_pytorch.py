9084d955905c56676ade6aaa014ad4c5c2e317d7
JiaqiLiZju <jiaqili@zju.edu.cn>
2020-07-31 19:59:40 +0800

    update_attention

---
+++ attention_pytorch.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+class EncoderDecoder(nn.Module):
+    """
+    A standard Encoder-Decoder architecture. Base for this and many 
+    other models.
+    """
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+        
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        "Take in and process masked src and target sequences."
+        return self.decode(self.encode(src, src_mask), src_mask,
+                            tgt, tgt_mask)
+    
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+    
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+
+class BiLSTM_Attention(nn.Module):
+    def __init__(self):
+        super(BiLSTM_Attention, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, n_hidden, bidirectional=True)
+        self.out = nn.Linear(n_hidden * 2, num_classes)
+
+    # lstm_output : [batch_size, n_step, n_hidden * num_directions(=2)], F matrix
+    def attention_net(self, lstm_output, final_state):
+        hidden = final_state.view(-1, n_hidden * 2, 1)   # hidden : [batch_size, n_hidden * num_directions(=2), 1(=n_layer)]
+        attn_weights = torch.bmm(lstm_output, hidden).squeeze(2) # attn_weights : [batch_size, n_step]
+        soft_attn_weights = F.softmax(attn_weights, 1)
+        # [batch_size, n_hidden * num_directions(=2), n_step] * [batch_size, n_step, 1] = [batch_size, n_hidden * num_directions(=2), 1]
+        context = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
+        return context, soft_attn_weights.data.numpy() # context : [batch_size, n_hidden * num_directions(=2)]
+
+    def forward(self, X):
+        input = self.embedding(X) # input : [batch_size, len_seq, embedding_dim]
+        input = input.permute(1, 0, 2) # input : [len_seq, batch_size, embedding_dim]
+
+        hidden_state = Variable(torch.zeros(1*2, len(X), n_hidden)) # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        cell_state = Variable(torch.zeros(1*2, len(X), n_hidden)) # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+
+        # final_hidden_state, final_cell_state : [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        output, (final_hidden_state, final_cell_state) = self.lstm(input, (hidden_state, cell_state))
+        output = output.permute(1, 0, 2) # output : [batch_size, len_seq, n_hidden]
+        attn_output, attention = self.attention_net(output, final_hidden_state)
+        return self.out(attn_output), attention # model : [batch_size, num_classes], attention : [batch_size, n_step]
